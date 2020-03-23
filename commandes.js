0aca407322c625dc687b98a1d2be4dd99b3f1fc8
HugoHUET <hugo.huet.14@gmail.com>
2020-03-23 11:35:21 +0100

    add commandes

--- commandes.js
+++ commandes.js
@@ -9,7 +9,6 @@ db.sports.updateOne({_id : ObjectId("5e7886d506b87bec15cfdf1e")}, {$set : {titre
 db.sports.updateMany({equipeNecessaire : true}, {$set : {joueursMinimum : 4}}, {upsert: true})
 db.sports.updateMany({equipeNecessaire : true}, {$inc : {joueursMinimum : 2}})
 db.sports.updateMany({}, {$set : {players : [] }}, {upsert: true})
-db.sports.updateMany({equipeNecessaire : true}, {$push : {"players" : { $each: [ "BigG", "Paul", "Samy" ] } } }).pretty()
-
-
-
+db.sports.updateMany({_id : ObjectId("5e7886d506b87bec15cfdf1e")}, {$push : {"players" : { $each: [ {nom : "BigG"}, {nom : "Paul"}, {nom : "Samy"} ] } } })
+db.sports.updateMany({_id : ObjectId("5e7886fc06b87bec15cfdf1f")}, {$push : {"players" : { $each: [ {nom : "Marie"}, {nom : "Robert"}, {nom : "Michel"} ] } } })
+db.sports.updateMany({equipeNecessaire : true}, {$set: { "players.$.titulaire": true }}, {upsert: true})
