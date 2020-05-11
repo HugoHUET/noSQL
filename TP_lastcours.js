1674972835cdfc620283451f4456d39d2b7b717f
HugoHUET <hugo.huet.14@gmail.com>
2020-05-11 16:01:04 +0200

    finition du TP

--- TP_lastcours.js
+++ TP_lastcours.js
@@ -1,4 +1,4 @@
-// 1. Prendre trois point (3 longitudes et latitudes)
+// 1. Prendre trois point dans Paris
 	longitude, latitude
 	48.871120, 2.360154
 	48.837490, 2.364986
@@ -7,11 +7,12 @@
 // 2. Choisir un point aléatoire et trouver le point le plus proche de celui-ci parmis les 3 premiers
 	48.858898, 2.294330
 
-// 3. Faire une zone comprenant 2 points MAXIMUM à l'intérieur parmis les 3 premiers points
+// 3. Faire une zone comprenant 2 points MAXIMUM à l'intérieur de celle-ci parmis les 3 premiers points
 
-// 4. Trouver le nombre de point dans un rayon autour du points choisis
+// 4. Trouver tout les points dans un rayon autour du point choisi
 	48.858898, 2.294330
 
+// Réponses :
 // 1.
 db.places.insertMany(
 	[{name : "Bistrot Lucien", location : {type : "Point", coordinates : [2.360154, 48.871120]}},
@@ -19,17 +20,17 @@ db.places.insertMany(
 	{name : "Arc de Triomphe", location : {type : "Point", coordinates : [2.295047, 48.873803]}}])
 db.places.createIndex( { location : "2dsphere" } )
 
-// 2.
+// 2. plusieurs possibilités
 db.places.aggregate([
     {
         $geoNear: {
             near: { type: "Point", coordinates: [2.294330, 48.858898] },
             spherical: true,
-            distanceField: "calcDistance"
+            distanceField: "dist"
         }
     },
     { $limit: 1 }
-])
+]).pretty()
 db.places.find(
     {
         location:
@@ -40,7 +41,7 @@ db.places.find(
             }
         }
     }
-).limit(1)
+).limit(1).pretty()
 db.places.findOne(
    {
      location: {
@@ -52,7 +53,7 @@ db.places.findOne(
         }
      }
    }
-).pretty()
+)
 
 // 3. 
 db.places.find({
@@ -81,7 +82,7 @@ db.places.find(
             $nearSphere: {
                 $geometry: {
                     type: "Point",
-                    coordinates: [48.858898, 2.294330]
+                    coordinates: [2.294330, 48.858898]
                 },
                 $maxDistance: 4000
             }
